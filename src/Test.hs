65e4638cc802826ca4c050b7c95257502c7b7007
Samuel Ariza <samargo0317@gmail.com>
2023-05-15 17:48:50 -0500

    Eliminating unused functions

--- src/Test.hs
+++ src/Test.hs
@@ -17,21 +17,11 @@ naturalToNat n = Succ (naturalToNat (n-1))
 prop_idR :: Natural -> Bool
 prop_idR n = natToNatural (idR (naturalToNat n))  == n
 
-
 prop_constR :: Natural -> Bool
 prop_constR n = natToNatural (constR (naturalToNat n)) == natToNatural (Succ Zero)
 
 prop_multR :: Natural -> Natural -> Bool
 prop_multR m n = natToNatural (multR (naturalToNat m) (naturalToNat n)) == m * n
-
-prop_expR :: Natural -> Natural -> Bool
-prop_expR m n = natToNatural (expR (naturalToNat m) (naturalToNat n)) == m ^ n
-
-prop_factR :: Natural -> Bool
-prop_factR m = natToNatural (factR (naturalToNat m)) == product [1..m]
-
-prop_factP :: Natural -> Bool
-prop_factP m = natToNatural (factP (naturalToNat m)) == product [1..m]
 
 prop_predR :: Natural -> Bool
 prop_predR m | m > 0 = natToNatural (predR (naturalToNat m)) == m-1
@@ -40,8 +30,6 @@ prop_predR m | m > 0 = natToNatural (predR (naturalToNat m)) == m-1
 prop_subR :: Natural -> Natural -> Bool
 prop_subR m n | m >= n = natToNatural (subR (naturalToNat m) (naturalToNat n)) == m-n
               | otherwise = natToNatural (subR (naturalToNat m) (naturalToNat n)) == 0
-
-
 
 main :: IO()
 main = do
