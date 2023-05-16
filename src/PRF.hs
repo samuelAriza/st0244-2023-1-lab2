ddac2153deff426e8adb3fb4b31f2579dfbb4ac9
Samuel Ariza <samargo0317@gmail.com>
2023-05-15 19:48:48 -0500

    Finalizing project

--- src/PRF.hs
+++ src/PRF.hs
@@ -1,39 +1,39 @@
 module PRF where
 
+{-Definition of the natural numbers.-}
 data Nat = Zero | Succ Nat
 
+{-recNat is the recursion operator that allows defining recursive functions on
+natural numbers.-}
 recNat :: a -> (Nat -> a -> a) -> Nat -> a
 recNat a _ Zero = a
 recNat a h (Succ n) = h n (recNat a h n)
 
---addition function
+{-addR uses recNat to recursively apply the successor function Succ to the
+ second argument n times, starting from the first argument m.-}
 addR :: Nat -> Nat -> Nat
 addR m n = recNat n (\_ y -> Succ y) m
 
---identity function
+{-idR uses recNat to recursively apply the successor function Succ to the
+second argument y times, starting from Zero.-}
 idR :: Nat -> Nat
 idR m = recNat Zero (\_ y -> Succ y) m
 
---constant functions
+{-constR uses recNat to return Succ Zero regardless of the input.-}
 constR :: Nat -> Nat
-constR m = recNat (Succ Zero) (\_ y -> Succ Zero) m
+constR m = recNat (Succ Zero) (\_ _ -> Succ Zero) m
 
---multiplication function
+{-multR uses recNat to recursively add the second argument n to the
+previous result y m times, starting from Zero.-}
 multR :: Nat -> Nat -> Nat
 multR m n = recNat Zero (\_ y -> addR n y) m
 
---predecessor function
+{- predR uses recNat to recursively return the previous result x without
+ applying any operation.-}
 predR :: Nat -> Nat
 predR m = recNat Zero (\x _ -> x) m
 
---truncated subtraction function
+{-Uses recNat to recursively subtract 1 from the previous result y n times, starting from
+m.-}
 subR :: Nat -> Nat -> Nat
 subR m n = recNat m (\_ y -> predR y) n
-
-
-
-
-
-
-
-
--- src/Test.hs
+++ src/Test.hs
@@ -1,36 +1,84 @@
-import PRF
-import Test.QuickCheck
-import Numeric.Natural (Natural)
+--Test module in which the tests are carried out
+{-Instructing the GHC compiler not to
+issue warnings related to orphan instances.-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
+{-Import the file containing the definitions
+of the five recursive primitive functions-}
+import PRF
+
+{-Import the Natural data type
+from the Numeric.Natural module.-}
+import Numeric.Natural ( Natural )
+
+{-Import QuickCheck-}
+import Test.QuickCheck
+  ( Arbitrary ( arbitrary, shrink )
+  , arbitrarySizedNatural
+  , quickCheck
+  , shrinkIntegral
+  )
+
+{-An instance of Arbitrary is
+defined for the type Natural-}
 instance Arbitrary Natural where
   arbitrary = arbitrarySizedNatural
-  shrink = shrinkIntegral
+  shrink    = shrinkIntegral
 
+{-Function that converts Nat to Natural-}
 natToNatural :: Nat -> Natural
 natToNatural Zero = 0
 natToNatural (Succ n) = 1 + natToNatural n
 
+{-Function that converts Natural to Nat-}
 naturalToNat :: Natural -> Nat
 naturalToNat 0 = Zero
 naturalToNat n = Succ (naturalToNat (n-1))
 
+{-Function from naturals to booleans that compares the identity
+function to return the same element it takes as an argument.-}
 prop_idR :: Natural -> Bool
 prop_idR n = natToNatural (idR (naturalToNat n))  == n
 
+{-Function from naturals to booleans that compares that
+the constant function, independent of the argument to
+which the function is applied, always returns Succ Zero or 1.-}
 prop_constR :: Natural -> Bool
-prop_constR n = natToNatural (constR (naturalToNat n)) == natToNatural (Succ Zero)
+prop_constR n =
+  natToNatural (constR (naturalToNat n)) == natToNatural (Succ Zero)
 
+{-Function that takes two naturals and returns a boolean.
+Compares the return converted to Natural of the recursive
+multiply function, which uses recNat, when applied to two random
+arguments with the normal multiplication of those two arguments
+using the infix operator (*).-}
 prop_multR :: Natural -> Natural -> Bool
-prop_multR m n = natToNatural (multR (naturalToNat m) (naturalToNat n)) == m * n
+prop_multR m n =
+  natToNatural (multR (naturalToNat m) (naturalToNat n)) == m * n
 
+{-Function that takes a natural and returns a boolean.
+Compare that the return converted to Natural
+obtained by the application of the predecessor function,
+is equal to the number m passed as argument of the function
+minus one, in case the argument is greater than 0 and compares
+that the return is equal to zero in case the argument
+is something else.-}
 prop_predR :: Natural -> Bool
 prop_predR m | m > 0 = natToNatural (predR (naturalToNat m)) == m-1
              | otherwise = natToNatural (predR (naturalToNat m)) == 0
 
+{-truncated subtraction function takes two arguments,
+in case m is greater than or equal to n it applies the
+function to m and n and compares that it is equal to
+the subtraction with the infix operator (-), otherwise it
+returns zero and compares it just with zero. -}
 prop_subR :: Natural -> Natural -> Bool
-prop_subR m n | m >= n = natToNatural (subR (naturalToNat m) (naturalToNat n)) == m-n
-              | otherwise = natToNatural (subR (naturalToNat m) (naturalToNat n)) == 0
+prop_subR m n | m >= n =
+  natToNatural (subR (naturalToNat m) (naturalToNat n)) == m-n
+              | otherwise =
+  natToNatural (subR (naturalToNat m) (naturalToNat n)) == 0
 
+{-Main function-}
 main :: IO()
 main = do
   quickCheck prop_idR
